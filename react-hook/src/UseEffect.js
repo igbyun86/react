1e9b7075aee54ab89fde0550718edf909c431072
igbyun86 <igbyun86@gmail.com>
2023-12-01 09:18:51 +0900

    Fix useEffect cleanUp 추가

--- react-hook/src/UseEffect.js
+++ react-hook/src/UseEffect.js
@@ -1,10 +1,23 @@
 import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 function UseEffect() {
     const [count, setCount] = useState(0);
     const [printCount, setPrintCount] = useState(0);
 
     useEffect(() => {
+        //컴포넌트 초기에 한번만 실행(componentDidMount)
+        console.log("componentDidMount!");
+        
+        //unmount(componentWillUnmount)
+        return () => {
+            console.log("clean up!!");
+        }
+    }, []);
+
+
+    useEffect(() => {
+        //componentDidUpdate(printCount)
         console.log(`You clicked ${printCount} times`);
     }, [printCount]);
 
@@ -18,6 +31,8 @@ function UseEffect() {
             <button onClick={() => setPrintCount(printCount + 1)}>
                 Click me
             </button>
+            <p></p>
+            <Link to="/useState"><p>useState</p></Link>
         </div>
     );
 }
